ca1fe60dea60237ca2dc43c8fa2e2e044891f65c
2017cs030 <chamoddinuka518@gmail.com>
2020-07-02 13:41:59 +0530

    cofiguration

--- web/src/component/admin/dashboard/dashboard.js
+++ web/src/component/admin/dashboard/dashboard.js
@@ -3,34 +3,10 @@ import './dashboard.css';
 import  firebase from '../../../firebase'
 
 class Dashboard extends Component{
-    state={
-        email:''
-    }
-    submito=(e)=>{
-        console.log(this.state.email)
-        const db = firebase.firestore();
-        db.collection("user").doc("LA").set({
-            email: this.state.email,
-            state: "CA",
-            
-        })
-        .then(function() {
-            console.log("Document successfully written!");
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        });
-    }
-    handle=(e)=>{
-        this.setState({email:e.target.value});
-        
-    }
-        render(){ 
+   render(){ 
         return(
             <div className="dashboard">
                 Dashboard
-            <input  onChange={this.handle} type="text"/>
-            <button type="submit" onClick={this.submito}>submit</button>
             </div>
            
             
