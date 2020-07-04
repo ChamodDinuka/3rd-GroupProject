acc71a791dcebe59e22ac6d386c55560095ac7e2
2017cs030 <chamoddinuka518@gmail.com>
2020-07-04 21:05:09 +0530

    notification list

--- web/src/component/admin/navbar/nabar.js
+++ web/src/component/admin/navbar/nabar.js
@@ -1,11 +1,13 @@
 import React,{Component} from 'react';
-import { Navbar,Nav } from 'react-bootstrap';
+import { Navbar,Nav,Dropdown } from 'react-bootstrap';
 import './navbar.css';
 import firebase from '../../../firebase'
 
 class nabar extends Component{
   state={
-    view:0
+    view:0,
+    list:'false',
+    user_reports:[]
   }
   componentDidMount(){
     
@@ -22,10 +24,23 @@ class nabar extends Component{
       this.setState({
         view:i
       });console.log(this.state.view)
-    }.bind(this)); 
-    
-    
+    }.bind(this));   
   }
+    getAll=()=>{
+      var db=firebase.firestore()
+      db.collection("reports").get().then(function(querySnapshot) {
+        var reports_type=[]
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            
+            reports_type.push(doc.data().report_type);
+        });
+        this.setState({
+          user_reports:reports_type
+        });
+    }.bind(this));
+   
+    }
     render(){
         return(
             
@@ -37,13 +52,28 @@ class nabar extends Component{
                 
               </Nav>
               <Nav>
-                <Nav.Link href="#deets"><i className="fa fa-bell-o" aria-hidden="true" ><a id="ico">{this.state.view}</a></i></Nav.Link>
+              <Dropdown>
+  <Dropdown.Toggle  id="dropdown-basic" >
+   {<i className="fa fa-bell-o" aria-hidden="true" onClick={this.getAll} ><a id="ico" >{this.state.view}</a></i>}
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+  {this.state.user_reports.map((answer, i) => {
+    return(
+     <Dropdown.Item href="#/action-1">Reported {this.state.user_reports[i]}</Dropdown.Item>
+    )
+   })}
+        
+  </Dropdown.Menu>
+</Dropdown>
+                
                 <Nav.Link eventKey={2} href="#memes">
                   Logout
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          
         );
     }
     
