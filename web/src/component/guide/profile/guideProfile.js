a08c9427832da313619a5b01cacc57acc7101287
2017cs030 <chamoddinuka518@gmail.com>
2020-08-02 13:10:03 +0530

    create chatbot folder

---
+++ web/src/component/guide/profile/guideProfile.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+
+class guideProfile extends Component{
+    render(){
+        return(
+            <></>
+        );
+    }
+}
+export default guideProfile;
