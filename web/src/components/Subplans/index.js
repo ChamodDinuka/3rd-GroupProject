e4667afaf2d59a2134d8be67b02befc7b21eee56
keshani97 <kalanikeshani@gmail.com>
2020-10-06 10:17:39 -0700

    package loading page

--- web/src/components/Subplans/index.js
+++ web/src/components/Subplans/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.css'
+import './Subplanstyle.css'
 import { Link } from 'react-router-dom';
 
 /**
@@ -10,42 +10,46 @@ import { Link } from 'react-router-dom';
 const Subplans = (props) => {
   return(
     <div>
-        
-        <div class="B-box">
-            <div class="box">
-                <div class="package">
-                <Link to="/BronzePlan" ><img src="/images/bronze4.png"></img></Link>
-                </div>
-                    <span>
-                        Bronze Package
-                    <p>This is good package for a lot of members
-                         in your team.you will receive hotel rooms 
-                         with a vehicle for 5 days</p>
-                    </span>
-            </div> 
-            <div class="box">
-                <div class="package1">
-                <Link to="/GoldPlan" ><img src="/images/gold.png"></img></Link>
-                </div>
-                    <span><h6>Gold Package</h6>
-                        <p>In this package you receive hotel 
-                            rooms and a vehicle for 5 days.select
-                             and get the features.</p>
+        <div class="row">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h5>Platinum Package </h5>
+                    </div>
+                    <div class="card-body">
+                        <p>Price $250</p>
+
+                        <button class="btn btn-primary">More</button>
+                        <button class="btn btn-primary">select</button> 
                         
-                    </span>
-            </div> 
-            <div class="box">
-                <div class="package1">
-                <Link to="/SilverPlan" ><img src="/images/silver.png"></img></Link>
+                    </div>
                 </div>
-                    <span>
-                    <h6>Silver Package</h6>
-                    <p>In this package you receive hotel rooms
-                         with no vehicles for 4 days with 
-                         reasonable price</p>
-                    </span>
-            </div>    
-        </div>   
+
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h5>Silver Package </h5>
+                    </div>
+                    <div class="card-body">
+                        <p>Price $500</p>
+                        <button class="btn btn-primary">More</button>
+                        <button class="btn btn-primary">select</button> 
+                    </div>
+                    
+                </div>
+
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h5>Gold Package </h5>
+                    </div>
+                    <div class="card-body">
+                        <p>Price $800</p>
+
+                        <button class="btn btn-primary">More</button>
+                        <button class="btn btn-primary">select</button> 
+                    </div>
+                </div>
+        </div>
+        
+           
     </div>
    )
 
